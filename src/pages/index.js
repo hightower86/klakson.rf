8bf8e3626bea1dbaa5fe638a1aa0461eaf768504
Adel <htwr86@gmail.com>
2019-10-01 17:33:22 +0300

    styling header and main page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,27 +9,18 @@ import "./mystyles.scss"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <section className="hero hero-index is-dark is-fullheight">
-      <div className="hero-body">
-        <div className="container has-text-centered">
-          <h1 className="title is-1">
-            СТО "КЛАКСОН" <br /> Пыть-Ях
-          </h1>
-          <h2 className="subtitle is-4">
-            Добро пожаловать на сайт нашего техцентра
-          </h2>
-          <h3 className="subtitle is-5">
-            Ваш автомобиль будет в надежных руках
-          </h3>
-          {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-            <Image />
-          </div> */}
-          <button className="button i-button">ЗАПИСЬ НА РЕМОНТ</button>
-        </div>
+    <section className="main-page">
+      <div className="container">
+        <h1 className="">
+          СТО "КЛАКСОН" <br /> Пыть-Ях
+        </h1>
+        <h2 className="">Добро пожаловать на сайт нашего техцентра</h2>
+        <h3 className="">Ваш автомобиль будет в надежных руках</h3>
+        <button className="button i-button">ЗАПИСЬ НА РЕМОНТ</button>
       </div>
     </section>
 
-    <section className="hero is-fullheight-with-navbar is-dark">
+    <section className="section-next">
       <div className="hero-body">
         <div className="container">
           <h2 className="title is-1"></h2>
